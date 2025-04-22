8e9ccd0b072e067f25cefb7676290b3720d873c9
Gianluca Mariani <gianluca.mariani.gm@gmail.com>
2025-04-22 23:02:29 +0200

    Removed old code not needed, added gitignore

--- find_equilibria.jl
+++ find_equilibria.jl
@@ -1,33 +1,5 @@
 # dynamic_lagrangian_equilibria.jl
 
-using Symbolics
-using HomotopyContinuation
-using DynamicPolynomials
-
-function build_lagrangian_gradient(n::Int)
-    @variables t ω r₀ r₁
-    @variables q[1:n]  # dynamic vector of q₁, q₂, ..., qₙ
-
-    # === Build the components ===
-
-    # time_factor = (-1)^i + r0 * sin(ω * t + phi(i))
-    phi = i -> (i % 4 == 0) || (i % 4 == 3) ? 0.0 : π  # example with if condition
-    time_factor = [(-1)^(i-1) + r₀ * sin(ω * t + phi(i-1)) for i in 1:n]
-
-    potential_quad = 0.5 * sum(time_factor[i] * q[i]^2 for i in 1:n)
-    potential_quartic = 0.25 * sum(q[i]^4 for i in 1:n)
-
-    # q_shifted is q rolled by +1 (circular shift)
-    q_shifted = [q[mod1(i - 1, n)] for i in 1:n]  # mod1: Julia is 1-based
-    potential_coupling = 0.5 * r₁ * sum((q[i] - q_shifted[i])^2 for i in 1:n)
-
-    # Lagrangian (actually negative potential)
-    V = potential_quad + potential_quartic + potential_coupling
-    gradV = Symbolics.gradient(V, q)
-
-    return -gradV, q, (t, ω, r₀, r₁)
-end
-
 using Symbolics, DynamicPolynomials, HomotopyContinuation
 
 function find_equilibria(n, t_val, ω_val, r0_val, r1_val)
@@ -62,4 +34,4 @@ function find_equilibria(n, t_val, ω_val, r0_val, r1_val)
 end
 
 # Example call
-find_equilibria(2, 1.0, 2.0, 0.5, 0.0)
+find_equilibria(4, 1.0, 2.0, 0.5, 0.0)
